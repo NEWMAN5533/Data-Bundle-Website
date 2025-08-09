7749fe58f58770bd004623374f290ac6bbece1d5
NEWMAN5533 <ilyasahmed553341@gmail.com>
2025-08-09 15:41:19 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -8,6 +8,70 @@ const bundleInput = document.getElementById('bundle');
 const priceInput = document.getElementById('price');
 const phoneInput = document.getElementById('phone');
 
+
+
+// WORKING HOURS TIMER //
+
+const hourHand = document.querySelector('.hourHand');
+
+const minuteHand = document.querySelector('.minuteHand');
+const secondHand = document.querySelector('.secondHand');
+const time = document.querySelector('.time');
+
+const clock = document.querySelector('.clock');
+
+function setDate() {
+  const today = new Date();
+
+  const second = today.getSeconds();
+
+  const minute = today.getMinutes();
+
+  const hour = today.getHours();
+
+  const secondDeg = ((second /  60) * 360) + 360;
+
+
+  const minuteDeg = ((minute / 60) * 360);
+
+  const hourDeg = ((hour / 12) * 360);
+
+  secondHand.style.transform = `rotate(${secondDeg}deg)`;
+
+  minuteHand.style.transform = `rotate(${minuteDeg}deg)`;
+
+  hourHand.style.transform = `rotate(${hourDeg}deg)`;
+
+  time.innerHTML = `<span></strong>${hour}</strong> : ${minute} : <small>${second}</small></span>`
+}
+
+setInterval(setDate, 1000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // LOOP THROUGH ALL BUY BUTTONS
 buyButtons.forEach(button => {
   button.addEventListener('click', () => {
